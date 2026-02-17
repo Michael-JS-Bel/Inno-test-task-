a19b2068a81987c2242c072e7e8df1512ef48aa3
Michail <astapenko.m99@gmail.com>
2026-02-17 16:42:35 +0300

    chore: setup project and format tools

---
+++ eslint.config.js
@@ -0,0 +1,38 @@
+import js from '@eslint/js';
+import { defineConfig, globalIgnores } from 'eslint/config';
+import eslintConfigPrettier from 'eslint-config-prettier';
+import simpleImportSort from 'eslint-plugin-simple-import-sort';
+import unicorn from 'eslint-plugin-unicorn';
+import globals from 'globals';
+
+export default defineConfig([
+  globalIgnores(['dist', 'node_modules']),
+
+  {
+    files: ['**/*.{js,mjs}'],
+
+    languageOptions: {
+      ecmaVersion: 2022,
+      sourceType: 'module',
+      globals: globals.browser,
+    },
+
+    plugins: {
+      'simple-import-sort': simpleImportSort,
+      unicorn,
+    },
+
+    extends: [js.configs.recommended, unicorn.configs.recommended, eslintConfigPrettier],
+
+    rules: {
+      'simple-import-sort/imports': 'warn',
+      'simple-import-sort/exports': 'warn',
+      'no-console': 'warn',
+      'no-debugger': 'error',
+      'unicorn/prefer-query-selector': 'off',
+      'unicorn/no-null': 'off',
+      'unicorn/prevent-abbreviations': 'off',
+      'no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+    },
+  },
+]);
